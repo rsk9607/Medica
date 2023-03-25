dc8da37aeb1573ae00f5d93274e6f994d13fbde4
rsk9607 <108059528+rsk9607@users.noreply.github.com>
2023-03-25 23:06:06 +0530

    update of Data for prompt

--- medica/medica_bot/Data/forms.py
+++ medica/medica_bot/Data/forms.py
@@ -1,13 +1,8 @@
 from django import forms
-from django.contrib.admin.widgets import AdminDateWidget
 from .models import Patient
 
 class medica_form(forms.ModelForm):
     class Meta:
         model = Patient
-        exclude = ['user','full_name']
-        widgets = {
-            'date_of_birth': forms.DateInput(attrs={'type': 'date', 'format': 'dd-mm-yyyy'}),
-        }
-        
+        fields = '__all__'
         
--- medica/medica_bot/Data/views.py
+++ medica/medica_bot/Data/views.py
@@ -2,11 +2,35 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Patient
 from django.shortcuts import redirect,render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 def register_medica(request):
+    researcher = ''
+    study = ''
     context ={}
-    template = loader.get_template('register_medica.html')
-    return HttpResponse(template.render(context, request))
+    if request.method == "POST":
+        user = request.user
+        full_name = request.user.first_name +''+ request.user.last_name
+        date_0f_birth = request.POST.get('date_of_birth')
+        address = request.POST.get('address')
+        gender = request.POST.get('gender')
+        phone_number = request.POST.get('phone_number')
+        emergency_contact_name = request.POST.get('emergency_contact_name')
+        emergency_contact_phone = request.POST.get('emergency_contact_phone')
+        medical_history = request.POST.get('medical_history')
+        height = request.POST.get('height')
+        weight = request.POST.get('weight')
+        physician_notes = request.POST.get('physician_notes')
+        
+        data = Patient.objects.create(user=user,full_name=full_name,date_of_birth=date_0f_birth,address=address,gender=gender,phone_number=phone_number,emergency_contact_name=emergency_contact_name,emergency_contact_phone=emergency_contact_phone,medical_history =medical_history,height=height,weight=weight,physician_notes=physician_notes)
+
+        data.save()
+        return redirect('/home')
+    else:    
+        template = loader.get_template('register_medica.html')
+        return HttpResponse(template.render(context, request))
 
 def profile(request):
     patient = Patient.objects.all().values()
